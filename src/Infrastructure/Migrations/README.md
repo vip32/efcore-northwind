2ce0e9427313275579e584ff79de47498e6ee57a
vp <mini32@gmail.com>
2021-01-18 21:10:29 +0100

    Add project files.

---
+++ src/Infrastructure/Migrations/README.md
@@ -0,0 +1,5 @@
+﻿- new migration: 
+`dotnet ef migrations add Initial --context NorthwindDbContext --output-dir Migrations --project src\Infrastructure --startup-project src\Presentation.Web --namespace Infrastructure`
+
+- update database: 
+`dotnet ef database update --project src\Infrastructure --startup-project src\Presentation.Web`
---
+++ src/Presentation.Web/Startup.cs
@@ -0,0 +1,61 @@
+using Infrastructure;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace Presentation.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<NorthwindDbContext>(options =>
+              options.UseSqlite("Filename=data_application.db"));
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Presentation.Web", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider.GetService<NorthwindDbContext>().Database.Migrate(); // auto migrate
+            }
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Presentation.Web v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
